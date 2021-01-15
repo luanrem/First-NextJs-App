a344a66b7c049b217c0f17cac6da7762dc20a0b9
luanrem <luanrem@gmail.com>
2021-01-14 22:23:16 -0300

    Client Side Fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
-import { Tytle } from '../styles/pages/Home'
+import { useEffect, useState } from 'react';
+import { Tytle } from '../styles/pages/Home';
+
+interface IProduct {
+  id: string;
+  title: string;
+};
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <Tytle>Hello World</Tytle>
+      <section>
+        <Tytle>Products</Tytle>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
   )
 }
