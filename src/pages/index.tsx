378719e5957a51500d0ffaa45478f4561d992dc4
luanrem <luanrem@gmail.com>
2021-01-14 22:13:00 -0300

    Styled Components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Tytle } from '../styles/pages/Home'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Tytle>Hello World</Tytle>
     </div>
   )
 }
---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Tytle = styled.h1`
+  color: #8257e5;
+`;
